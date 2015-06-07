9d8c2130493dcf1e708f4257950e17ee55832b93
robneu <rob@wpsitecare.com>
2015-06-06 23:59:14 -0500

    Added entry title functions

--- functions/template-entry.php
+++ functions/template-entry.php
@@ -10,6 +10,95 @@
  */
 
 /**
+ * This is a "private" helper function used to format the entry title's display.
+ * If a link is passed into it, the title will be wrapped in an anchor tag which
+ * links to the desired URI.
+ *
+ * This technically can be used by theme developers, but it isn't recommended as
+ * we make no guarantee of backwards compatibility in the future.
+ *
+ * @since  0.2.0
+ * @access private
+ * @param  $id mixed the desired title's post id
+ * @param  $link string the desired title's link URI
+ * @return string
+ */
+function _sitecare_get_formatted_title( $id = '', $link = '' ) {
+	$post_id = empty( $id ) ? get_the_ID() : $id;
+	$title   = get_the_title( absint( $post_id ) );
+
+	if ( empty( $link ) ) {
+		return $title;
+	}
+
+	if ( get_permalink() === $link && get_the_ID() !== $post_id ) {
+		$link = get_permalink( absint( $post_id ) );
+	}
+
+	return sprintf( '<a href="%s" rel="bookmark" itemprop="url">%s</a>',
+		esc_url( $link ),
+		$title
+	);
+}
+
+/**
+ * This is a wrapper function for get_the_title which allows theme developers to
+ * grab a formatted version of the post title without needing to add a lot of
+ * extra markup in template files.
+ *
+ * By default, all entry titles except the main title on single entries are
+ * wrapped in an anchor tag pointed to the post's permalink.
+ *
+ * @since  0.2.0
+ * @access public
+ * @param  $args array
+ * @return string
+ */
+function sitecare_get_entry_title( $args = array() ) {
+	$is_main  = is_singular() && is_main_query();
+	$defaults = apply_filters( 'sitecare_entry_title_defaults',
+		array(
+			'tag'     => $is_main ? 'h1' : 'h2',
+			'attr'    => 'entry-title',
+			'link'    => $is_main ? '' : get_permalink(),
+			'post_id' => get_the_ID(),
+			'before'  => '',
+			'after'   => '',
+		)
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$id   = isset( $args['post_id'] ) ? $args['post_id'] : '';
+	$attr = isset( $args['attr'] ) ? hybrid_get_attr( $args['attr'] ) : '';
+	$link = isset( $args['link'] ) ? $args['link'] : '';
+
+	$output = isset( $args['before'] ) ? $args['before'] : '';
+
+	$output .= sprintf( '<%1$s %2$s>%3$s</%1$s>',
+		$args['tag'],
+		$attr,
+		_sitecare_get_formatted_title( $id, $link )
+	);
+
+	$output .= isset( $args['after'] ) ? $args['after'] : '';
+
+	return apply_filters( 'sitecare_entry_title', $output, $args );
+}
+
+/**
+ * Outputs a formatted entry title.
+ *
+ * @since  0.2.0
+ * @access public
+ * @param  $args array
+ * @return void
+ */
+function sitecare_entry_title( $args = array() ) {
+	echo sitecare_get_entry_title( $args );
+}
+
+/**
  * This is simply a wrapper function for hybrid_get_post_author which adds a few
  * filters to make the function a bit more flexible. This will allow us to avoid
  * passing args into the function by default in our templates. Instead, we can
