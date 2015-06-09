f4332084dcda2c6d8c80b8bc1829d6ebad6f9d4d
robneu <rob@wpsitecare.com>
2015-06-08 19:01:09 -0500

    Added missing theme hooks

--- functions/template.php
+++ functions/template.php
@@ -30,3 +30,35 @@ function sitecare_content_template_hierarchy( $templates ) {
 	}
 	return $templates;
 }
+
+/**
+ * Temporarily adding some placeholder hook locations until they're added to
+ * Theme Hook Alliance. Hopefully these won't be in here very long.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+if ( ! function_exists( 'tha_content_while_before' ) ) {
+	function tha_content_while_before() {
+		do_action( 'tha_content_while_before' );
+	}
+}
+
+if ( ! function_exists( 'tha_content_while_after' ) ) {
+	function tha_content_while_after() {
+		do_action( 'tha_content_while_after' );
+	}
+}
+
+if ( ! function_exists( 'tha_entry_content_before' ) ) {
+	function tha_entry_content_before() {
+		do_action( 'tha_entry_content_before' );
+	}
+}
+
+if ( ! function_exists( 'tha_entry_content_after' ) ) {
+	function tha_entry_content_after() {
+		do_action( 'tha_entry_content_after' );
+	}
+}
