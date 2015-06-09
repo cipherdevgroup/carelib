0d52c52983face67995daa6a8b71d4e0dbfc3a84
robneu <rob@wpsitecare.com>
2015-06-09 14:12:45 -0500

    Created a psudo namespace with classes
    Since I don’t want to move beyond PHP 5.2.4 support until core does,
    this should suffice as a namespace.

--- functions/attr.php
+++ functions/attr.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * HTML attribute functions and filters. The purposes of this is to provide a
- * way for theme/plugin devs to hook into the attributes for specific HTML
- * elements and create new or modify existing attributes. The biggest benefit of
- * using this is to provide richer microdata while being forward compatible with
- * the ever-changing Web.  Currently, the default microdata vocabulary supported
- * is Schema.org.
+ * Small adjustments to the default Hybrid Core attributes. Changes include
+ * the addition of some markup elements, a change to the entry summary class,
+ * and a secondary nav element for non-site nav menus.
  *
  * @package     SiteCareLibrary
  * @subpackage  HybridCore
@@ -14,92 +11,115 @@
  * @since       0.1.0
  */
 
-// Attributes for major structural elements.
-add_filter( 'hybrid_attr_site-container', 'sitecare_attr_site_container' );
-add_filter( 'hybrid_attr_site-inner',     'sitecare_attr_site_inner' );
-add_filter( 'hybrid_attr_wrap',           'sitecare_attr_wrap', 10, 2 );
-// Post-specific attributes.
-add_filter( 'hybrid_attr_entry-summary',  'sitecare_attr_entry_summary_class' );
-// Other attributes.
-add_filter( 'hybrid_attr_nav',            'sitecare_attr_nav', 10, 2 );
-
 /**
- * Page site container element attributes.
- *
- * @since  0.1.0
- * @access public
- * @param  array $attr
- * @return array
+ * SiteCare Attributes class.
  */
-function sitecare_attr_site_container( $attr ) {
-	$attr['id']    = 'site-container';
-	$attr['class'] = 'site-container';
-	return $attr;
-}
+class SiteCare_Attributes {
 
-/**
- * Page site inner element attributes.
- *
- * @since  0.1.0
- * @access public
- * @param  array $attr
- * @return array
- */
-function sitecare_attr_site_inner( $attr ) {
-	$attr['id']    = 'site-inner';
-	$attr['class'] = 'site-inner';
-	return $attr;
-}
+	/**
+	* Get our class up and running!
+	*
+	* @since  0.1.0
+	* @access public
+	* @return void
+	*/
+	public function run() {
+		self::wp_hooks();
+	}
 
-/**
- * Page wrap element attributes.
- *
- * @since  0.1.0
- * @access public
- * @param  array $attr
- * @return array
- */
-function sitecare_attr_wrap( $attr, $context ) {
-	if ( empty( $context ) ) {
+	/**
+	* Register our actions and filters.
+	*
+	* @since  0.1.0
+	* @access public
+	* @return void
+	*/
+	private function wp_hooks() {
+		add_filter( 'hybrid_attr_site-container', array( $this, 'site_container' ) );
+		add_filter( 'hybrid_attr_site-inner',     array( $this, 'site_inner' ) );
+		add_filter( 'hybrid_attr_wrap',           array( $this, 'wrap' ), 10, 2 );
+		add_filter( 'hybrid_attr_entry-summary',  array( $this, 'entry_summary_class' ) );
+		add_filter( 'hybrid_attr_nav',            array( $this, 'nav' ), 10, 2 );
+	}
+
+	/**
+	 * Page site container element attributes.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array $attr
+	 * @return array
+	 */
+	public function site_container( $attr ) {
+		$attr['id']    = 'site-container';
+		$attr['class'] = 'site-container';
 		return $attr;
 	}
-	$attr['class'] = "wrap {$context}-wrap";
-	return $attr;
-}
 
-/**
- * Post summary/excerpt attributes.
- *
- * @since  0.1.0
- * @access public
- * @param  array $attr
- * @return array
- */
-function sitecare_attr_entry_summary_class( $attr ) {
-	$attr['class'] = 'entry-content summary';
-	return $attr;
-}
-
-/**
- * Attributes for nav elements which aren't necessarily site navigation menus.
- * One example use case for this would be pagination and page link blocks.
- *
- * @since  0.1.0
- * @access public
- * @param  array   $attr
- * @param  string  $context
- * @return array
- */
-function sitecare_attr_nav( $attr, $context ) {
-	$class = 'nav';
-
-	if ( ! empty( $context ) ) {
-		$attr['id'] = "nav-{$context}";
-		$class    .= " nav-{$context}";
+	/**
+	 * Page site inner element attributes.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array $attr
+	 * @return array
+	 */
+	public function site_inner( $attr ) {
+		$attr['id']    = 'site-inner';
+		$attr['class'] = 'site-inner';
+		return $attr;
 	}
 
-	$attr['class'] = $class;
-	$attr['role']  = 'navigation';
+	/**
+	 * Page wrap element attributes.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array $attr
+	 * @return array
+	 */
+	public function wrap( $attr, $context ) {
+		if ( empty( $context ) ) {
+			return $attr;
+		}
+		$attr['class'] = "wrap {$context}-wrap";
+		return $attr;
+	}
 
-	return $attr;
+	/**
+	 * Post summary/excerpt attributes.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array $attr
+	 * @return array
+	 */
+	public function entry_summary_class( $attr ) {
+		$attr['class'] = 'entry-content summary';
+		return $attr;
+	}
+
+	/**
+	 * Attributes for nav elements which aren't necessarily site navigation menus.
+	 * One example use case for this would be pagination and page link blocks.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array   $attr
+	 * @param  string  $context
+	 * @return array
+	 */
+	public function nav( $attr, $context ) {
+		$class = 'nav';
+
+		if ( ! empty( $context ) ) {
+			$attr['id'] = "nav-{$context}";
+			$class    .= " nav-{$context}";
+		}
+
+		$attr['class'] = $class;
+		$attr['role']  = 'navigation';
+
+		return $attr;
+	}
 }
--- functions/seo.php
+++ functions/seo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tweaks and Adjustments for the Hybrid Core Framework.
+ * SEO Adjustments for the Hybrid Core Framework.
  *
  * @package     SiteCareLibrary
  * @subpackage  HybridCore
@@ -9,48 +9,73 @@
  * @since       0.1.0
  */
 
-// Remove unwanted default Hybrid head elements.
-remove_action( 'wp_head',  'hybrid_meta_template', 1 );
-
-add_filter( 'hybrid_site_title', 'sitecare_seo_site_title' );
 /**
- * Returns the linked site title wrapped in a `<p>` tag unless on the home page
- * or the main blog page where no other H1 exists.
- *
- * @since  0.1.0
- * @access public
- * @param  string $title
- * @return string
+ * SiteCare SEO class.
  */
-function sitecare_seo_site_title( $title ) {
-	if ( is_front_page() || is_home() ) {
-		return $title;
+class SiteCare_SEO {
+
+	/**
+	* Get our class up and running!
+	*
+	* @since  0.1.0
+	* @access public
+	* @return void
+	*/
+	public function run() {
+		self::wp_hooks();
 	}
-	return str_replace( array( '<h1', '</h1' ), array( '<p', '</p' ), $title );
-}
 
-add_filter( 'hybrid_site_description', 'sitecare_seo_site_description' );
-/**
- * Returns the site description wrapped in a `<p>` tag.
- *
- * @since  0.1.0
- * @access public
- * @param  string $desc
- * @return string
- */
-function sitecare_seo_site_description( $desc ) {
-	return str_replace( array( '<h2', '</h2' ), array( '<p', '</p' ), $desc );
-}
+	/**
+	* Register our actions and filters.
+	*
+	* @since  0.1.0
+	* @access public
+	* @return void
+	*/
+	private function wp_hooks() {
+		remove_action( 'wp_head', 'hybrid_meta_template', 1 );
+		add_filter( 'hybrid_site_title',       array( $this, 'site_title' ),       10 );
+		add_filter( 'hybrid_site_description', array( $this, 'site_description' ), 10 );
+		add_filter( 'excerpt_more',            array( $this, 'excerpt_more' ),     99 );
+	}
 
-add_filter( 'excerpt_more', 'sitecare_seo_excerpt_more' );
-/**
- * Filter the default Hybrid more link to add a rel="nofollow" attribute.
- *
- * @since  0.1.0
- * @access public
- * @param  string $text
- * @return string
- */
-function sitecare_seo_excerpt_more( $text ) {
-	return str_replace( 'class="more-link"', 'rel="nofollow" class="more-link"', $text );
+	/**
+	 * Returns the linked site title wrapped in a `<p>` tag unless on the home page
+	 * or the main blog page where no other H1 exists.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  string $title
+	 * @return string
+	 */
+	public function site_title( $title ) {
+		if ( is_front_page() || is_home() ) {
+			return $title;
+		}
+		return str_replace( array( '<h1', '</h1' ), array( '<p', '</p' ), $title );
+	}
+
+	/**
+	 * Returns the site description wrapped in a `<p>` tag.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  string $desc
+	 * @return string
+	 */
+	public function site_description( $desc ) {
+		return str_replace( array( '<h2', '</h2' ), array( '<p', '</p' ), $desc );
+	}
+
+	/**
+	 * Filter the default Hybrid more link to add a rel="nofollow" attribute.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  string $text
+	 * @return string
+	 */
+	public function excerpt_more( $text ) {
+		return str_replace( 'class="more-link"', 'rel="nofollow" class="more-link"', $text );
+	}
 }
