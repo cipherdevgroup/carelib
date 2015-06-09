5dabd835591a90534e065a4b75184d8145ac44ac
robneu <rob@wpsitecare.com>
2015-06-09 00:42:26 -0500

    We don't use this function anymore
    No reason to keep filtering it

--- functions/template.php
+++ functions/template.php
@@ -9,28 +9,6 @@
  * @since       0.1.0
  */
 
-add_filter( 'hybrid_content_template_hierarchy', 'sitecare_content_template_hierarchy' );
-/**
- * Search the template paths and replace them with singular and archive versions.
- *
- * By default, the content template hierarchy forces you to add logic for single
- * and archive content within the templates themselves. This makes the templates
- * overly complex and I would prefer to separate them into individual files.
- *
- * @since  0.1.0
- * @access public
- * @param  array $templates
- * @return array $templates
- */
-function sitecare_content_template_hierarchy( $templates ) {
-	if ( is_singular() || is_attachment() ) {
-		$templates = str_replace( 'content/', 'content/singular/', $templates );
-	} else {
-		$templates = str_replace( 'content/', 'content/archive/', $templates );
-	}
-	return $templates;
-}
-
 /**
  * Temporarily adding some placeholder hook locations until they're added to
  * Theme Hook Alliance. Hopefully these won't be in here very long.
