b34a2e2e297a07c44991db7e4db47cec9f018ac0
robneu <rob@wpsitecare.com>
2015-07-06 14:39:06 -0500

    Removed textdomain override
    The textdomain will be replaced on build. This is useless.

--- inc/functions-i18n.php
+++ inc/functions-i18n.php
@@ -20,9 +20,6 @@ add_action( 'after_setup_theme', 'carelib_load_locale_functions', 0 );
 # Load translations for theme, child theme, and framework.
 add_action( 'after_setup_theme', 'carelib_load_textdomains', 5 );
 
-# Overrides the load textdomain function for the 'carelib' domain.
-add_filter( 'override_load_textdomain', 'carelib_override_load_textdomain', 5, 3 );
-
 # Filter the textdomain mofile to allow child themes to load the parent theme translation.
 add_filter( 'load_textdomain_mofile', 'carelib_load_textdomain_mofile', 10, 2 );
 
@@ -37,8 +34,6 @@ add_filter( 'load_textdomain_mofile', 'carelib_load_textdomain_mofile', 10, 2 );
  * @return void
  */
 function carelib_load_locale_functions() {
-
-	// Get the site's locale.
 	$locale = strtolower( str_replace( '_', '-', get_locale() ) );
 
 	// Define locale functions files.
@@ -46,12 +41,14 @@ function carelib_load_locale_functions() {
 	$theme_func = HYBRID_PARENT . carelib_get_parent_domain_path() . "/{$locale}.php";
 
 	// If file exists in child theme.
-	if ( is_child_theme() && file_exists( $child_func ) )
+	if ( is_child_theme() && file_exists( $child_func ) ) {
 		require_once( $child_func );
+	}
 
 	// If file exists in parent theme.
-	if ( file_exists( $theme_func ) )
+	if ( file_exists( $theme_func ) ) {
 		require_once( $theme_func );
+	}
 }
 
 /**
@@ -65,63 +62,14 @@ function carelib_load_locale_functions() {
  * @return void
  */
 function carelib_load_textdomains() {
-
 	// Load theme textdomain.
 	load_theme_textdomain( carelib_get_parent_textdomain(), HYBRID_PARENT . carelib_get_parent_domain_path() );
 
 	// Load child theme textdomain.
-	if ( is_child_theme() )
+	if ( is_child_theme() ) {
 		load_child_theme_textdomain( carelib_get_child_textdomain(), HYBRID_CHILD . carelib_get_child_domain_path() );
-
-	// Load the framework textdomain.
-	carelib_load_framework_textdomain();
-}
-
-/**
- * Overrides the load textdomain functionality when 'carelib' is the domain in use.  The purpose of
- * this is to allow theme translations to handle the framework's strings.  What this function does is
- * sets the 'carelib' domain's translations to the theme's.  That way, we're not loading multiple
- * of the same MO files.
- *
- * @since  2.0.0
- * @access public
- * @globl  array   $l10n
- * @param  bool    $override
- * @param  string  $domain
- * @param  string  $mofile
- * @return bool
- */
-function carelib_override_load_textdomain( $override, $domain, $mofile ) {
-
-	// Check if the domain is one of our framework domains.
-	if ( 'carelib' === $domain ) {
-		global $l10n;
-
-		// Get the theme's textdomain.
-		$theme_textdomain = carelib_get_parent_textdomain();
-
-		// If the theme's textdomain is loaded, use its translations instead.
-		if ( $theme_textdomain && isset( $l10n[ $theme_textdomain ] ) )
-			$l10n[ $domain ] = $l10n[ $theme_textdomain ];
-
-		// Always override.  We only want the theme to handle translations.
-		$override = true;
 	}
 
-	return $override;
-}
-
-/**
- * Loads an empty MO file for the framework textdomain.  This will be overwritten.  The framework domain
- * will be merged with the theme domain.
- *
- * @since  1.3.0
- * @access public
- * @param  string $domain The name of the framework's textdomain.
- * @return bool           Whether the MO file was loaded.
- */
-function carelib_load_framework_textdomain( $domain = 'carelib' ) {
-	return load_textdomain( $domain, '' );
 }
 
 /**
@@ -169,8 +117,9 @@ function carelib_get_child_textdomain() {
 	global $hybrid;
 
 	// If a child theme isn't active, return an empty string.
-	if ( !is_child_theme() )
+	if ( ! is_child_theme() ) {
 		return '';
+	}
 
 	// If the global textdomain isn't set, define it. Plugin/theme authors may also define a custom textdomain.
 	if ( empty( $hybrid->child_textdomain ) ) {
@@ -207,9 +156,9 @@ function carelib_get_parent_domain_path() {
  * @return string
  */
 function carelib_get_child_domain_path() {
-
-	if ( !is_child_theme() )
+	if ( ! is_child_theme() ) {
 		return '';
+	}
 
 	$theme = wp_get_theme();
 
@@ -228,9 +177,8 @@ function carelib_get_child_domain_path() {
  * @return string
  */
 function carelib_load_textdomain_mofile( $mofile, $domain ) {
-
 	// If the $domain is for the parent or child theme, search for a $domain-$locale.mo file.
-	if ( $domain == carelib_get_parent_textdomain() || $domain == carelib_get_child_textdomain() ) {
+	if ( $domain === carelib_get_parent_textdomain() || $domain === carelib_get_child_textdomain() ) {
 
 		// Get the locale.
 		$locale = get_locale();
@@ -259,9 +207,9 @@ function carelib_load_textdomain_mofile( $mofile, $domain ) {
  * @return string
  */
 function carelib_get_language( $locale = '' ) {
-
-	if ( empty( $locale ) )
+	if ( empty( $locale ) ) {
 		$locale = get_locale();
+	}
 
 	return sanitize_key( preg_replace( '/(.*?)_.*?$/i', '$1', $locale ) );
 }
@@ -276,9 +224,9 @@ function carelib_get_language( $locale = '' ) {
  * @return string
  */
 function carelib_get_region( $locale = '' ) {
-
-	if ( empty( $locale ) )
+	if ( empty( $locale ) ) {
 		$locale = get_locale();
+	}
 
 	return sanitize_key( preg_replace( '/.*?_(.*?)$/i', '$1', $locale ) );
 }
