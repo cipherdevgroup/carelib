6a388a9e10e489789b5827d747c0a640ab8dddbf
robneu <rob@wpsitecare.com>
2015-07-07 00:31:25 -0500

    No subpackage

--- carelib.php
+++ carelib.php
@@ -3,7 +3,6 @@
  * Load all required library files.
  *
  * @package    CareLib
- * @subpackage HybridCore
  * @copyright  Copyright (c) 2015, WP Site Care, LLC
  * @license    GPL-2.0+
  * @since      0.1.0
--- inc/class-site-logo.php
+++ inc/class-site-logo.php
@@ -5,7 +5,6 @@
  * Based on the Jetpack site logo feature.
  *
  * @package    CareLib
- * @subpackage HybridCore
  * @copyright  Copyright (c) 2015, WP Site Care, LLC
  * @license    GPL-2.0+
  * @since      0.1.0
