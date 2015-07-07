55c5ac5d1cd9f9c16982cc0fb57374e6146bd698
robneu <rob@wpsitecare.com>
2015-07-06 23:27:07 -0500

    Moved utility filters into filters class

--- inc/class-filters.php
+++ inc/class-filters.php
@@ -61,6 +61,13 @@ class CareLib_Filters {
 		# Don't strip tags on single post titles.
 		remove_filter( 'single_post_title', 'strip_tags' );
 
+		# Filters the title for untitled posts.
+		add_filter( 'the_title', array( $this, 'untitled_post' ) );
+
+		# Filters the archive title and description.
+		add_filter( 'get_the_archive_title',       array( $this, 'archive_title_filter' ),       5 );
+		add_filter( 'get_the_archive_description', array( $this, 'archive_description_filter' ), 5 );
+
 		# Use same default filters as 'the_content' with a little more flexibility.
 		add_filter( "{$this->prefix}_archive_description", array( $wp_embed, 'run_shortcode' ), 5 );
 		add_filter( "{$this->prefix}_archive_description", array( $wp_embed, 'autoembed' ),     5 );
@@ -234,5 +241,112 @@ class CareLib_Filters {
 	public function comments_popup_link_attributes( $attr ) {
 		return 'itemprop="discussionURL"';
 	}
+
+	/**
+	 * The WordPress.org theme review requires that a link be provided to the single post page for untitled
+	 * posts. This is a filter on 'the_title' so that an '(Untitled)' title appears in that scenario, allowing
+	 * for the normal method to work.
+	 *
+	 * @since  1.6.0
+	 * @access public
+	 * @param  string  $title
+	 * @return string
+	 */
+	public function untitled_post( $title ) {
+		// Translators: Used as a placeholder for untitled posts on non-singular views.
+		if ( ! $title && ! is_singular() && in_the_loop() && ! is_admin() ) {
+			$title = esc_html__( '(Untitled)', 'carelib' );
+		}
+
+		return $title;
+	}
+
+	/**
+	 * Filters `get_the_archve_title` to add better archive titles than core.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  string  $title
+	 * @return string
+	 */
+	public function archive_title_filter( $title ) {
+		if ( is_home() && !is_front_page() )
+			$title = get_post_field( 'post_title', get_queried_object_id() );
+
+		elseif ( is_category() )
+			$title = single_cat_title( '', false );
+
+		elseif ( is_tag() )
+			$title = single_tag_title( '', false );
+
+		elseif ( is_tax() )
+			$title = single_term_title( '', false );
+
+		elseif ( is_author() )
+			$title = carelib_get_single_author_title();
+
+		elseif ( is_search() )
+			$title = carelib_get_search_title();
+
+		elseif ( is_post_type_archive() )
+			$title = post_type_archive_title( '', false );
+
+		elseif ( get_query_var( 'minute' ) && get_query_var( 'hour' ) )
+			$title = carelib_get_single_minute_hour_title();
+
+		elseif ( get_query_var( 'minute' ) )
+			$title = carelib_get_single_minute_title();
+
+		elseif ( get_query_var( 'hour' ) )
+			$title = carelib_get_single_hour_title();
+
+		elseif ( is_day() )
+			$title = carelib_get_single_day_title();
+
+		elseif ( get_query_var( 'w' ) )
+			$title = carelib_get_single_week_title();
+
+		elseif ( is_month() )
+			$title = single_month_title( ' ', false );
+
+		elseif ( is_year() )
+			$title = carelib_get_single_year_title();
+
+		elseif ( is_archive() )
+			$title = carelib_get_single_archive_title();
+
+		return apply_filters( 'carelib_archive_title', $title );
+	}
+
+	/**
+	 * Filters `get_the_archve_description` to add better archive descriptions than core.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  string  $desc
+	 * @return string
+	 */
+	public function archive_description_filter( $desc ) {
+		if ( is_home() && !is_front_page() )
+			$desc = get_post_field( 'post_content', get_queried_object_id(), 'raw' );
+
+		elseif ( is_category() )
+			$desc = get_term_field( 'description', get_queried_object_id(), 'category', 'raw' );
+
+		elseif ( is_tag() )
+			$desc = get_term_field( 'description', get_queried_object_id(), 'post_tag', 'raw' );
+
+		elseif ( is_tax() )
+			$desc = get_term_field( 'description', get_queried_object_id(), get_query_var( 'taxonomy' ), 'raw' );
+
+		elseif ( is_author() )
+			$desc = get_the_author_meta( 'description', get_query_var( 'author' ) );
+
+		elseif ( is_post_type_archive() )
+			$desc = get_post_type_object( get_query_var( 'post_type' ) )->description;
+
+		return apply_filters( 'carelib_archive_description', $desc );
+	}
+
 
 }
