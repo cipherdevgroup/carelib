db82bff62fa0b721353b7ff334e19bf13c36a6cf
robneu <rob@wpsitecare.com>
2015-07-08 02:13:30 -0500

    Sidebar class

--- inc/functions-sidebars.php
+++ inc/functions-sidebars.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Helper functions for working with the WordPress sidebar system. Currently, the framework creates a
- * simple function for registering HTML5-ready sidebars instead of the default WordPress unordered lists.
+ * Helper functions for working with the WordPress sidebar system.
+ *
+ * Currently, the framework creates a simple method for registering
+ * HTML5-ready sidebars instead of the default WordPress unordered lists.
  *
  * @package   CareLib
  * @copyright Copyright (c) 2015, WP Site Care, LLC
@@ -13,53 +15,94 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Wrapper function for WordPress' register_sidebar() function. This function exists so that theme authors
- * can more quickly register sidebars with an HTML5 structure instead of having to write the same code
- * over and over. Theme authors are also expected to pass in the ID, name, and description of the sidebar.
- * This function can handle the rest at that point.
- *
- * @since  0.2.0
- * @access public
- * @param  array   $args
- * @return string  Sidebar ID.
+ * CareLib Template Post Class.
  */
-function carelib_register_sidebar( $args ) {
+class CareLib_Sidebar {
 
-	// Set up some default sidebar arguments.
-	$defaults = array(
-		'id'            => '',
-		'name'          => '',
-		'description'   => '',
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-	);
+	/**
+	 * Library prefix which can be set within themes.
+	 *
+	 * @since 0.2.0
+	 * @var   string
+	 */
+	protected $prefix;
 
-	// Parse the arguments.
-	$args = wp_parse_args( $args, apply_filters( 'carelib_sidebar_defaults', $defaults ) );
+	/**
+	 * Constructor method.
+	 *
+	 * @since 0.2.0
+	 */
+	public function __construct() {
+		$this->prefix = CareLib::instance()->get_prefix();
+	}
 
-	// Remove action.
-	remove_action( 'widgets_init', '__return_false', 95 );
+	/**
+	 * Get our class up and running!
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function run() {
+		self::wp_hooks();
+	}
 
-	// Register the sidebar.
-	return register_sidebar( apply_filters( 'carelib_sidebar_args', $args ) );
-}
+	/**
+	 * Register our actions and filters.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function wp_hooks() {
+		add_action( 'widgets_init', '__return_false', 95 );
+	}
 
-# Compatibility for when a theme doesn't register any sidebars.
-add_action( 'widgets_init', '__return_false', 95 );
+	/**
+	 * Wrapper function for WordPress' register_sidebar() function.
+	 *
+	 * This function exists so that theme authors can more quickly register
+	 * sidebars with an HTML5 structure instead of having to write the same code
+	 * over and over. Theme authors are also expected to pass in the ID, name,
+	 * and description of the sidebar.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  array   $args
+	 * @return string  Sidebar ID.
+	 */
+	public function register( $args ) {
+		$defaults = apply_filters( "{$this->prefix}_sidebar_defaults", array(
+			'id'            => '',
+			'name'          => '',
+			'description'   => '',
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		) );
 
-/**
- * Function for grabbing a dynamic sidebar name.
- *
- * @since  0.2.0
- * @access public
- * @global array   $wp_registered_sidebars
- * @param  string  $sidebar_id
- * @return string
- */
-function carelib_get_sidebar_name( $sidebar_id ) {
-	global $wp_registered_sidebars;
+		$args = wp_parse_args( $args, $defaults );
 
-	return isset( $wp_registered_sidebars[ $sidebar_id ] ) ? $wp_registered_sidebars[ $sidebar_id ]['name'] : '';
+		remove_action( 'widgets_init', '__return_false', 95 );
+
+		// Register the sidebar.
+		return register_sidebar( apply_filters( "{$this->prefix}_sidebar_args", $args ) );
+	}
+
+	/**
+	 * Function for grabbing a dynamic sidebar name.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @global array   $wp_registered_sidebars
+	 * @param  string  $sidebar_id
+	 * @return string
+	 */
+	public function get_name( $sidebar_id ) {
+		global $wp_registered_sidebars;
+
+		return isset( $wp_registered_sidebars[ $sidebar_id ] ) ? $wp_registered_sidebars[ $sidebar_id ]['name'] : false;
+	}
+
 }
