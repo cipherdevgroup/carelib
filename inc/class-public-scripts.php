76492b596c7f22e3570b16d7caf015ad50a007da
robneu <rob@wpsitecare.com>
2015-07-08 20:34:42 -0500

    Split public scripts into standalone class

--- inc/class-public-scripts.php
+++ inc/class-public-scripts.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * Methods for handling JavaScript and CSS in the framework.
- *
- * Themes can add support for the 'hybrid-core-scripts' feature to allow the
- * framework to handle loading the stylesheets into the theme header or footer
- * at an appropriate time.
+ * Methods for handling front-end JavaScript and CSS in the framework.
  *
  * @package   CareLib
  * @copyright Copyright (c) 2015, WP Site Care, LLC
@@ -18,23 +14,7 @@ defined( 'ABSPATH' ) || exit;
 /**
  * CareLib Translations class.
  */
-class CareLib_Scripts {
-
-	/**
-	 * Library prefix which can be set within themes.
-	 *
-	 * @since 0.2.0
-	 * @var   string
-	 */
-	protected $prefix;
-
-	/**
-	 * Script suffix to determine whether or not to load minified scripts.
-	 *
-	 * @since 0.2.0
-	 * @var   string
-	 */
-	protected $suffix;
+class CareLib_Public_Scripts extends CareLib_Scripts {
 
 	/**
 	 * Library prefix which can be set within themes.
@@ -74,12 +54,11 @@ class CareLib_Scripts {
 	 * @since 0.2.0
 	 */
 	public function __construct() {
-		$this->prefix     = CareLib::instance()->get_prefix();
-		$this->suffix     = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 		$this->parent     = trailingslashit( get_template_directory() );
 		$this->child      = trailingslashit( get_stylesheet_directory() );
 		$this->parent_uri = trailingslashit( get_template_directory_uri() );
 		$this->child_uri  = trailingslashit( get_stylesheet_directory_uri() );
+		parent::__construct();
 	}
 
 	/**
@@ -111,35 +90,20 @@ class CareLib_Scripts {
 	}
 
 	/**
-	 * Helper function for getting the script/style `.min` suffix for minified files.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @return string
-	 */
-	public function get_suffix() {
-		return $this->suffix;
-	}
-
-	/**
-	 * Tells WordPress to load the scripts needed for the framework using the
-	 * wp_enqueue_script() function.
+	 * Enqueue front-end scripts for the framework.
 	 *
 	 * @since  0.2.0
 	 * @access public
 	 * @return void
 	 */
 	public function enqueue_scripts() {
-		// Load the comment reply script on singular posts with open comments if threaded comments are supported.
 		if ( is_singular() && get_option( 'thread_comments' ) && comments_open() ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 	}
 
 	/**
-	 * Registers stylesheets for the framework. This function merely registers styles with WordPress using
-	 * the wp_register_style() function. It does not load any stylesheets on the site. If a theme wants to
-	 * register its own custom styles, it should do so on the 'wp_enqueue_scripts' hook.
+	 * Register front-end stylesheets for the framework.
 	 *
 	 * @since  1.5.0
 	 * @access public
@@ -151,8 +115,9 @@ class CareLib_Scripts {
 	}
 
 	/**
-	 * Returns the parent theme stylesheet URI. Will return the active theme's stylesheet URI if no child
-	 * theme is active. Be sure to check `is_child_theme()` when using.
+	 * Returns the parent theme stylesheet URI. Will return the active theme's
+	 * stylesheet URI if no child theme is active. Be sure to check
+	 * `is_child_theme()` when using.
 	 *
 	 * @since  0.2.0
 	 * @access public
@@ -171,15 +136,15 @@ class CareLib_Scripts {
 	}
 
 	/**
-	 * Filters the 'stylesheet_uri' to allow theme developers to offer a minimized version of their main
-	 * 'style.css' file. It will detect if a 'style.min.css' file is available and use it if SCRIPT_DEBUG
-	 * is disabled.
+	 * Filters the 'stylesheet_uri' to allow theme developers to offer a
+	 * minimized version of their main 'style.css' file. It will detect if a
+	 * 'style.min.css' file is available and use it if SCRIPT_DEBUG is disabled.
 	 *
 	 * @since  1.5.0
 	 * @access public
-	 * @param  string  $stylesheet_uri      The URI of the active theme's stylesheet.
-	 * @param  string  $stylesheet_dir_uri  The directory URI of the active theme's stylesheet.
-	 * @return string  $stylesheet_uri
+	 * @param  string $stylesheet_uri The URI of the active theme's stylesheet.
+	 * @param  string $stylesheet_dir_uri The directory URI of the active theme's stylesheet.
+	 * @return string $stylesheet_uri
 	 */
 	public function min_stylesheet_uri( $stylesheet_uri, $stylesheet_dir_uri ) {
 		// Use the .min stylesheet if available.
@@ -197,14 +162,11 @@ class CareLib_Scripts {
 			}
 		}
 
-		// Return the theme stylesheet.
 		return $stylesheet_uri;
 	}
 
 	/**
-	 * Retrieves the file with the highest priority that exists. The function searches both the stylesheet
-	 * and template directories. This function is similar to the locate_template() function in WordPress
-	 * but returns the file name with the URI path instead of the directory path.
+	 * Retrieve the theme file with the highest priority that exists.
 	 *
 	 * @since  1.5.0
 	 * @access public
@@ -215,7 +177,6 @@ class CareLib_Scripts {
 	protected function locate_theme_file( $file_names ) {
 		$located = '';
 
-		// Loops through each of the given file names.
 		foreach ( (array) $file_names as $file ) {
 			// If the file exists in the stylesheet (child theme) directory.
 			if ( is_child_theme() && file_exists( $this->child . $file ) ) {
@@ -233,25 +194,40 @@ class CareLib_Scripts {
 	}
 
 	/**
-	 * Filters `locale_stylesheet_uri` with a more robust version for checking locale/language/region/direction
-	 * stylesheets.
+	 * Filters the 'stylesheet_uri' and checks if a post has a style that should
+	 * overwrite the theme's primary `style.css`.
 	 *
 	 * @since  0.2.0
 	 * @access public
 	 * @param  string  $stylesheet_uri
 	 * @return string
 	 */
-	public function locale_stylesheet_uri( $stylesheet_uri ) {
-		$locale_style = $this->get_locale_style();
-		return $locale_style ? esc_url( $locale_style ) : $stylesheet_uri;
+	public function style_filter( $stylesheet_uri ) {
+		if ( ! is_singular() ) {
+			return $stylesheet_uri;
+		}
+		$style = $this->get_post_style( get_queried_object_id() );
+
+		if ( $style && $style_uri = $this->locate_theme_file( array( $style ) ) ) {
+			$stylesheet_uri = $style_uri;
+		}
+
+		return $stylesheet_uri;
 	}
 
 	/**
-	 * Searches for a locale stylesheet. This function looks for stylesheets in the `css` folder in the following
-	 * order:  1) $lang-$region.css, 2) $region.css, 3) $lang.css, and 4) $text_direction.css. It first checks
-	 * the child theme for these files. If they are not present, it will check the parent theme. This is much
-	 * more robust than the WordPress locale stylesheet, allowing for multiple variations and a more flexible
-	 * hierarchy.
+	 * Searches for a locale stylesheet. This function looks for stylesheets in
+	 * the `css` folder in the following order:
+	 *
+	 * 1) $lang-$region.css,
+	 * 2) $region.css,
+	 * 3) $lang.css,
+	 * 4) $text_direction.css.
+	 *
+	 * It first checks the child theme for these files. If they are not present,
+	 * it will check the parent theme. This is much more robust than the
+	 * WordPress locale stylesheet, allowing for multiple variations and a more
+	 * flexible hierarchy.
 	 *
 	 * @since  0.2.0
 	 * @access public
@@ -282,88 +258,18 @@ class CareLib_Scripts {
 	}
 
 	/**
-	 * Filters the 'stylesheet_uri' and checks if a post has a style that should overwrite the theme's
-	 * primary `style.css`.
+	 * Filters `locale_stylesheet_uri` with a more robust version for checking
+	 * locale/language/region/direction stylesheets.
 	 *
 	 * @since  0.2.0
 	 * @access public
 	 * @param  string  $stylesheet_uri
 	 * @return string
 	 */
-	public function style_filter( $stylesheet_uri ) {
-		if ( ! is_singular() ) {
-			return $stylesheet_uri;
-		}
-		$style = $this->get_post_style( get_queried_object_id() );
+	public function locale_stylesheet_uri( $stylesheet_uri ) {
+		$locale_style = $this->get_locale_style();
 
-		if ( $style && $style_uri = $this->locate_theme_file( array( $style ) ) ) {
-			$stylesheet_uri = $style_uri;
-		}
-
-		return $stylesheet_uri;
-	}
-
-	/**
-	 * Gets a post style.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  int     $post_id
-	 * @return bool
-	 */
-	public function get_post_style( $post_id ) {
-		return get_post_meta( $post_id, $this->get_style_meta_key(), true );
-	}
-
-	/**
-	 * Sets a post style.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  int     $post_id
-	 * @param  string  $layout
-	 * @return bool
-	 */
-	public function set_post_style( $post_id, $style ) {
-		return update_post_meta( $post_id, $this->get_style_meta_key(), $style );
-	}
-
-	/**
-	 * Deletes a post style.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  int     $post_id
-	 * @return bool
-	 */
-	public function delete_post_style( $post_id ) {
-		return delete_post_meta( $post_id, $this->get_style_meta_key() );
-	}
-
-	/**
-	 * Checks a post if it has a specific style.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  int     $post_id
-	 * @return bool
-	 */
-	public function has_post_style( $style, $post_id = '' ) {
-		if ( empty( $post_id ) ) {
-			$post_id = get_the_ID();
-		}
-		return $this->get_post_style( $post_id ) === $style ? true : false;
-	}
-
-	/**
-	 * Wrapper function for returning the metadata key used for objects that can use styles.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @return string
-	 */
-	public function get_style_meta_key() {
-		return apply_filters( "{$this->prefix}_style_meta_key", 'Stylesheet' );
+		return $locale_style ? esc_url( $locale_style ) : $stylesheet_uri;
 	}
 
 }
