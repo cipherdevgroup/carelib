3c581082dbc8bb4639531d34872560dbd5c02612
robneu <rob@wpsitecare.com>
2015-09-09 03:42:57 -0500

    Removed extra controls from JS as well

--- js/customize-controls.js
+++ js/customize-controls.js
@@ -1,34 +1,9 @@
 (function( $, api ) {
+	'use strict';
 
-	/* === Checkbox Multiple Control === */
-
-	api.controlConstructor['checkbox-multiple']= api.Control.extend({
-		ready: function() {
-			var control = this;
-
-			$( 'input:checkbox', control.container ).change(
-				function() {
-
-					// Get all of the checkbox values.
-					var checkbox_values = $( 'input[type="checkbox"]:checked', control.container ).map(
-						function() {
-							return this.value;
-						}
-					).get();
-
-					// Set the value.
-					if ( null === checkbox_values ) {
-						control.setting.set( '' );
-					} else {
-						control.setting.set( checkbox_values );
-					}
-				}
-			);
-		}
-	});
-
-	/* === Palette Control === */
-
+	/**
+	 * Palette Control
+	 */
 	api.controlConstructor.palette = api.Control.extend({
 		ready: function() {
 			var control = this;
@@ -38,7 +13,6 @@
 
 			$( 'input:radio', control.container ).change(
 				function() {
-
 					// Removes the `.selected` class from other labels and adds it to the new one.
 					$( 'label.selected', control.container ).removeClass( 'selected' );
 					$( this ).parent( 'label' ).addClass( 'selected' );
@@ -49,9 +23,10 @@
 		}
 	});
 
-	/* === Radio Image Control === */
-
-	api.controlConstructor['radio-image']= api.Control.extend({
+	/**
+	 * Radio Image Control
+	 */
+	api.controlConstructor['radio-image'] = api.Control.extend({
 		ready: function() {
 			var control = this;
 
@@ -62,39 +37,4 @@
 			);
 		}
 	});
-
-	/* === Select Group Control === */
-
-	api.controlConstructor['select-group']= api.Control.extend({
-		ready: function() {
-			var control = this;
-
-			$( 'select', control.container ).change(
-				function() {
-					control.setting.set( $( this ).val() );
-				}
-			);
-		}
-	});
-
-	/* === Select Multiple Control === */
-
-	api.controlConstructor['select-multiple']= api.Control.extend({
-		ready: function() {
-			var control = this;
-
-			$( 'select', control.container ).change(
-				function() {
-					var value = $( this ).val();
-
-					if ( null === value ) {
-						control.setting.set( '' );
-					} else {
-						control.setting.set( value );
-					}
-				}
-			);
-		}
-	});
-
 })( jQuery, wp.customize );
