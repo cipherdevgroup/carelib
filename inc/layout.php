163eb00afbc395b9d2cab5cc8a436ce1ba032ebc
robneu <rob@wpsitecare.com>
2015-09-08 23:57:50 -0500

    I don't like magic getters/setters

--- inc/layout.php
+++ inc/layout.php
@@ -32,65 +32,6 @@ class CareLib_Layout {
 	protected $args = array();
 
 	/**
-	 * Magic method for getting layout object properties.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  string  $property
-	 * @return mixed
-	 */
-	public function __get( $property ) {
-		return isset( $this->args[ $property ] ) ? $this->args[ $property ] : null;
-	}
-
-	/**
-	 * Magic method for setting layout object properties.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  string  $property
-	 * @param  mixed   $value
-	 * @return void
-	 */
-	public function __set( $property, $value ) {
-		$this->args[ $property ] = $value;
-	}
-
-	/**
-	 * Magic method for checking if a layout property is set.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  string  $property
-	 * @return bool
-	 */
-	public function __isset( $property ) {
-		return isset( $this->args[ $property ] );
-	}
-
-	/**
-	 * Don't allow properties to be unset.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @param  string  $property
-	 * @return void
-	 */
-	public function __unset( $property ) {}
-
-	/**
-	 * Magic method to use in case someone tries to output the layout object as a string.
-	 * We'll just return the layout name.
-	 *
-	 * @since  0.2.0
-	 * @access public
-	 * @return string
-	 */
-	public function __toString() {
-		return $this->name;
-	}
-
-	/**
 	 * Register a new layout object
 	 *
 	 * @since  0.2.0
@@ -110,22 +51,34 @@ class CareLib_Layout {
 	public function __construct( $name, $args = array() ) {
 		$name = sanitize_key( $name );
 
-		$defaults = array(
-			'is_global_layout' => true,    // Whether to show as an option in the customizer.
-			'is_post_layout'   => true,    // Whether to show as an option in the meta box.
-			'is_user_layout'   => true,    // Whether to show as an option in user profile (not implemented).
-			'label'            => $name,   // Internationalized text label.
-			'image'            => '',      // Image URL of the layout design.
-			'post_types'       => array(), // Array of post types layout works with.
-			'_builtin'         => false,   // Internal use only! Whether the layout is built in.
-			'_internal'        => false,   // Internal use only! Whether the layout is internal (cannot be unregistered).
-		);
-
-		$this->args = wp_parse_args( $args, $defaults );
+		$this->args = wp_parse_args( $args, array(
+			'label'            => $name,
+			'image'            => '',
+			'is_global_layout' => true,
+			'is_post_layout'   => true,
+			'is_user_layout'   => true,
+			'_builtin'         => false,
+			'_internal'        => false,
+			'post_types'       => array(),
+		) );
 
 		$this->args['name'] = $name;
 
-		$this->add_post_type_support();
+		if ( ! empty( $this->post_types ) ) {
+			$this->add_post_type_support();
+		}
+	}
+
+	/**
+	 * Magic method to return the layout name if someone tries to output the
+	 * layout object as a string.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->name;
 	}
 
 	/**
@@ -137,15 +90,194 @@ class CareLib_Layout {
 	 * @return void
 	 */
 	protected function add_post_type_support() {
-		if ( empty( $this->post_types ) ) {
-			return;
-		}
-
-		foreach ( $this->post_types as $post_type ) {
+		foreach ( (array) $this->post_types as $post_type ) {
 			if ( ! post_type_supports( $post_type, 'theme-layouts' ) ) {
 				add_post_type_support( $post_type, 'theme-layouts' );
 			}
 		}
+	}
+
+	/**
+	 * Return the layout name.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return string|bool false if no name has been set.
+	 */
+	public function get_name() {
+		return isset( $this->args['name'] ) ? (string) $this->args['name'] : false;
+	}
+
+	/**
+	 * Set the layout name.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  string $value
+	 * @return string
+	 */
+	public function set_name( $value ) {
+		$this->args['name'] = (string) $value;
+	}
+
+	/**
+	 * Return the layout label.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return string|bool false if no label has been set.
+	 */
+	public function get_label() {
+		return isset( $this->args['label'] ) ? (string) $this->args['label'] : false;
+	}
+
+	/**
+	 * Set the layout label.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  string $value
+	 * @return string
+	 */
+	public function set_label( $value ) {
+		$this->args['label'] = (string) $value;
+	}
+
+	/**
+	 * Return the layout image.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return string|bool false if no image has been set.
+	 */
+	public function get_image() {
+		return isset( $this->args['image'] ) ? (string) $this->args['image'] : false;
+	}
+
+	/**
+	 * Set the layout image.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  string $value
+	 * @return string
+	 */
+	public function set_image( $value ) {
+		$this->args['image'] = (string) $value;
+	}
+
+	/**
+	 * Check whether or not the current layout is a global layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function is_global() {
+		return isset( $this->args['is_global_layout'] ) ? (bool) $this->args['is_global_layout'] : false;
+	}
+
+	/**
+	 * Set the global layout property for the current layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  bool $value
+	 * @return bool
+	 */
+	public function set_is_global( $value ) {
+		$this->args['is_global_layout'] = (bool) $value;
+	}
+
+	/**
+	 * Check whether or not the current layout is a post layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function is_post() {
+		return isset( $this->args['is_post_layout'] ) ? (bool) $this->args['is_post_layout'] : false;
+	}
+
+	/**
+	 * Set the post layout property for the current layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  bool $value
+	 * @return bool
+	 */
+	public function set_is_post( $value ) {
+		$this->args['is_post_layout'] = (bool) $value;
+	}
+
+	/**
+	 * Check whether or not the current layout is a user layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function is_user() {
+		return isset( $this->args['is_user_layout'] ) ? (bool) $this->args['is_user_layout'] : false;
+	}
+
+	/**
+	 * Set the user layout property for the current layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  bool $value
+	 * @return bool
+	 */
+	public function set_is_user( $value ) {
+		$this->args['is_user_layout'] = (bool) $value;
+	}
+
+	/**
+	 * Check whether or not the current layout is a built-in layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function is_bultin() {
+		return isset( $this->args['_builtin'] ) ? (bool) $this->args['_builtin'] : false;
+	}
+
+	/**
+	 * Check whether or not the current layout is an internal layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return bool
+	 */
+	public function is_internal() {
+		return isset( $this->args['_internal'] ) ? (bool) $this->args['_internal'] : false;
+	}
+
+	/**
+	 * Get the supported post types property for the current layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return array
+	 */
+	public function get_post_types() {
+		return isset( $this->args['post_types'] ) ? (array) $this->args['post_types'] : array();
+	}
+
+	/**
+	 * Set the supported post types property for the current layout.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @param  array $value
+	 * @return array
+	 */
+	public function set_post_types( $value ) {
+		$this->args['post_types'] = (array) $value;
 	}
 
 }
