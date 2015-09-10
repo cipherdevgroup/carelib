1be2bcecd3a1385b8fd78054353cc118e8d0befc
robneu <rob@wpsitecare.com>
2015-09-10 01:01:17 -0500

    Moved theme support for post-thumbnails to theme support class

--- inc/support.php
+++ inc/support.php
@@ -62,6 +62,7 @@ class CareLib_Support {
 	 */
 	public function theme_support() {
 		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'html5', array(
 			'caption',
 			'comment-form',
