11d5a3c57e8b1f56e9ad5774f14394aec414fbae
robneu <rob@wpsitecare.com>
2015-09-11 03:52:42 -0500

    Removed unnecessary param

--- inc/image-grabber-api.php
+++ inc/image-grabber-api.php
@@ -19,14 +19,12 @@ class CareLib_Image_Grabber_API extends CareLib_Image_Grabber {
 	 * @param  array $args Arguments for how to load and display an image.
 	 * @return string|array the raw image string or an array of image attributes
 	 */
-	public function get_the_image( $args, $image = '' ) {
+	public function get( $args ) {
 		if ( ! $args = $this->setup_args( $args ) ) {
 			return false;
 		}
 
-		if ( empty( $image ) ) {
-			$image = $this->find_the_image( $args );
-		}
+		$image = $this->find_the_image( $args );
 
 		if ( is_array( $image ) ) {
 			$image = $this->format_image( $args, $image );
