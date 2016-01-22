8222a21a3d89823eef3ed5291fe2e777629db6bd
robneu <rob@wpsitecare.com>
2016-01-22 01:31:45 -0500

    Removed unused palette css/js

--- js/customize-controls.js
+++ js/customize-controls.js
@@ -2,28 +2,6 @@
 	'use strict';
 
 	/**
-	 * Palette Control
-	 */
-	api.controlConstructor.palette = api.Control.extend({
-		ready: function() {
-			var control = this;
-
-			// Adds a `.selected` class to the label of checked inputs.
-			$( 'input:radio:checked', control.container ).parent( 'label' ).addClass( 'selected' );
-
-			$( 'input:radio', control.container ).change(
-				function() {
-					// Removes the `.selected` class from other labels and adds it to the new one.
-					$( 'label.selected', control.container ).removeClass( 'selected' );
-					$( this ).parent( 'label' ).addClass( 'selected' );
-
-					control.setting.set( $( this ).val() );
-				}
-			);
-		}
-	});
-
-	/**
 	 * Radio Image Control
 	 */
 	api.controlConstructor['radio-image'] = api.Control.extend({
