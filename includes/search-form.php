2a844d65f166bbceb8e84d239ab0a4891357f6d5
robneu <rob@wpsitecare.com>
2016-01-31 02:31:43 -0500

    Simplified search form filtering method

--- includes/search-form.php
+++ includes/search-form.php
@@ -20,10 +20,9 @@
  * @return string
  */
 function carelib_search_form_get_form() {
-	return sprintf(
-		'<form class="search-form" method="get" action="%s" role="search">%s</form>',
+	return sprintf( '<form class="search-form" method="get" action="%s" role="search">%s</form>',
 		esc_url( home_url( '/' ) ),
-		_carelib_search_form_get_label() . _carelib_search_form_get_input() . _carelib_search_form_get_button()
+		_carelib_search_form_get_input() . _carelib_search_form_get_button()
 	);
 }
 
@@ -34,33 +33,21 @@ function carelib_search_form_get_form() {
  * @access protected
  * @return string
  */
-function _carelib_search_form_get_label() {
-	$label = apply_filters( "{$GLOBALS['carelib_prefix']}_search_form_label", __( 'Search site', 'carelib' ) );
-
-	return sprintf(
-		'<label id="%1$s-label" for="%1$s" class="screen-reader-text">%2$s</label>',
-		esc_attr( _carelib_search_form_get_id() ),
-		esc_attr( $label )
-	);
-}
-
-/**
- * Get the search form input field.
- *
- * @since  0.1.0
- * @access protected
- * @return string
- */
 function _carelib_search_form_get_input() {
-	$value = get_search_query() ? apply_filters( 'the_search_query', get_search_query() ) : '';
-	$placeholder = apply_filters( "{$GLOBALS['carelib_prefix']}_search_text", __( 'Search this website', 'carelib' ) );
+	$id = uniqid( 'searchform-' );
 
-	return sprintf(
-		'<input type="search" name="s" id="%s" placeholder="%s" autocomplete="off" value="%s" />',
-		esc_attr( _carelib_search_form_get_id() ),
-		esc_attr( $placeholder ),
-		esc_attr( $value )
+	$label = sprintf( '<label id="%1$s-label" for="%1$s" class="screen-reader-text">%2$s</label>',
+		esc_attr( $id ),
+		esc_attr( apply_filters( "{$GLOBALS['carelib_prefix']}_search_form_label", __( 'Search site', 'carelib' ) ) )
 	);
+
+	$input = sprintf( '<input type="search" name="s" id="%s" placeholder="%s" autocomplete="off" value="%s" />',
+		esc_attr( $id ),
+		esc_attr( apply_filters( "{$GLOBALS['carelib_prefix']}_search_text", __( 'Search this website', 'carelib' ) ) ),
+		esc_attr( apply_filters( 'the_search_query', get_search_query() ? get_search_query(): '' ) )
+	);
+
+	return $label . $input;
 }
 
 /**
@@ -71,28 +58,12 @@ function _carelib_search_form_get_input() {
  * @return string
  */
 function _carelib_search_form_get_button() {
-	return sprintf(
-		'<button type="submit" aria-label="%1$s">%2$s</button>',
+	return sprintf( '<button type="submit" aria-label="%1$s">%2$s</button>',
 		esc_attr( apply_filters( "{$GLOBALS['carelib_prefix']}_search_button_label", __( 'Search', 'carelib' ) ) ),
-		apply_filters( "{$GLOBALS['carelib_prefix']}_search_button_text", sprintf( '<span class="screen-reader-text">%s</span>',
-			esc_html__( 'Search', 'carelib' )
-		) )
+		apply_filters( "{$GLOBALS['carelib_prefix']}_search_button_text",
+			sprintf( '<span class="screen-reader-text">%s</span>',
+				esc_html__( 'Search', 'carelib' )
+			)
+		)
 	);
-}
-
-/**
- * Generate a unique ID for each search form.
- *
- * @since  0.1.0
- * @access protected
- * @return string
- */
-function _carelib_search_form_get_id() {
-	$id = false;
-
-	if ( ! $id ) {
-		$id = uniqid( 'searchform-' );
-	}
-
-	return $id;
 }
