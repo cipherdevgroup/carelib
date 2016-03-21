9080336b830669d5a3bc0a5748f72dbb4d1a3c7d
robneu <rob@wpsitecare.com>
2016-03-21 16:05:07 -0400

    Adjusted URI stuff

--- includes/paths.php
+++ includes/paths.php
@@ -19,35 +19,16 @@
  * @return string
  */
 function carelib_get_dir( $path = '' ) {
-	return CARELIB_DIR . ltrim( $path );
-}
-
-/**
- * Fix asset directory path on Windows installations.
- *
- * Because we don't know where the library is located, we need to
- * generate a URI based on the library directory path. In order to do
- * this, we are replacing the theme root directory portion of the
- * library directory with the theme root URI.
- *
- * @since  1.0.0
- * @access protected
- * @param  string $path the absolute path to the library's root directory.
- * @uses   trailingslashit()
- * @uses   get_template_directory()
- * @uses   get_theme_root_uri()
- * @return string a normalized uri string.
- */
-function _carelib_normalize_uri( $path ) {
-	return str_replace(
-		wp_normalize_path( get_theme_root() ),
-		get_theme_root_uri(),
-		wp_normalize_path( $path )
-	);
+	return CARELIB_DIR . ltrim( $path, '/' );
 }
 
 /**
  * Return the URI to the CareLib directory with a trailing slash.
+ *
+ * Because we don't know where the library is located, we need to generate a
+ * URI based on the library directory path. In order for this to work in all
+ * situations, including on Windows and when the library or theme have been
+ * symlinked into place, we're doing some fairly odd stuff.
  *
  * @since  1.0.0
  * @access public
@@ -55,7 +36,10 @@ function _carelib_normalize_uri( $path ) {
  * @return string
  */
 function carelib_get_uri( $path = '' ) {
-	return _carelib_normalize_uri( CARELIB_DIR ) . ltrim( $path );
+	$carelib_path = (string) apply_filters( 'carelib_uri_path', '/includes/vendor/' );
+	$carelib_uri = untrailingslashit( get_template_directory_uri() ) . $carelib_path . basename( CARELIB_DIR ) . '/';
+
+	return esc_url( $carelib_uri ) . ltrim( $path, '/' );
 }
 
 /**
@@ -67,7 +51,7 @@ function carelib_get_uri( $path = '' ) {
  * @return string
  */
 function carelib_get_css_uri( $path ) {
-	return carelib_get_uri( 'css/' ) . ltrim( $path );
+	return carelib_get_uri( 'css/' ) . ltrim( $path, '/' );
 }
 
 /**
@@ -79,7 +63,7 @@ function carelib_get_css_uri( $path ) {
  * @return string
  */
 function carelib_get_js_uri( $path ) {
-	return carelib_get_uri( 'js/' ) . ltrim( $path );
+	return carelib_get_uri( 'js/' ) . ltrim( $path, '/' );
 }
 
 /**
@@ -91,7 +75,7 @@ function carelib_get_js_uri( $path ) {
  * @return string
  */
 function carelib_get_parent_dir( $path = '' ) {
-	return PARENT_THEME_DIR . ltrim( $path );
+	return PARENT_THEME_DIR . ltrim( $path, '/' );
 }
 
 /**
@@ -103,7 +87,7 @@ function carelib_get_parent_dir( $path = '' ) {
  * @return string
  */
 function carelib_get_parent_uri( $path = '' ) {
-	return PARENT_THEME_URI . ltrim( $path );
+	return PARENT_THEME_URI . ltrim( $path, '/' );
 }
 
 /**
@@ -115,7 +99,7 @@ function carelib_get_parent_uri( $path = '' ) {
  * @return string
  */
 function carelib_get_child_dir( $path = '' ) {
-	return CHILD_THEME_DIR . ltrim( $path );
+	return CHILD_THEME_DIR . ltrim( $path, '/' );
 }
 
 /**
@@ -127,5 +111,5 @@ function carelib_get_child_dir( $path = '' ) {
  * @return string
  */
 function carelib_get_child_uri( $path = '' ) {
-	return CHILD_THEME_URI . ltrim( $path );
+	return CHILD_THEME_URI . ltrim( $path, '/' );
 }
