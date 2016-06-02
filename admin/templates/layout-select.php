ad79c800468d1b5ac64908e3a9d1796c6c60ac19
robneu <rob@wpsitecare.com>
2016-06-02 15:49:04 -0400

    Moved label

--- admin/templates/layout-select.php
+++ admin/templates/layout-select.php
@@ -9,9 +9,7 @@
  */
 ?>
 <label class="theme-layout">
-	<span class="screen-reader-text"><?php echo esc_html( $layout['label'] ); ?></span>
-
 	<input type="radio" value="<?php echo esc_attr( $name ); ?>" name="carelib-post-layout" <?php checked( $current_layout, $name ); ?> />
-
+	<span class="screen-reader-text"><?php echo esc_html( $layout['label'] ); ?></span>
 	<img src="<?php echo esc_url( sprintf( $layout['image'], get_template_directory_uri(), get_stylesheet_directory_uri() ) ); ?>" alt="<?php echo esc_attr( $layout['label'] ); ?>" />
 </label>
