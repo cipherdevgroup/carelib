60b01e66d51e07f8f6bca7436caf6f67761e8440
robneu <rob@wpsitecare.com>
2016-07-08 13:02:52 -0400

    Documented admin actions

--- admin/actions.php
+++ admin/actions.php
@@ -10,15 +10,72 @@
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Callback defined in includes/admin/scripts.php
+ *
+ * @see carelib_admin_register_scripts
+ */
 add_action( 'admin_enqueue_scripts',  'carelib_admin_register_scripts', 0 );
+
+/**
+ * Callback defined in includes/admin/styles.php
+ *
+ * @see carelib_admin_register_styles
+ */
 add_action( 'admin_enqueue_scripts', 'carelib_admin_register_styles', 0 );
-add_action( 'load-post.php',     'carelib_metabox_post_template_actions' );
+
+/**
+ * Callback defined in includes/admin/metabox-post-templates.php
+ *
+ * @see carelib_metabox_post_template_actions
+ */
+add_action( 'load-post.php', 'carelib_metabox_post_template_actions' );
+
+/**
+ * Callback defined in includes/admin/metabox-post-templates.php
+ *
+ * @see carelib_metabox_post_template_actions
+ */
 add_action( 'load-post-new.php', 'carelib_metabox_post_template_actions' );
-add_action( 'load-post.php',     'carelib_metabox_post_styles_actions' );
+
+/**
+ * Callback defined in includes/admin/metabox-post-styles.php
+ *
+ * @see carelib_metabox_post_styles_actions
+ */
+add_action( 'load-post.php', 'carelib_metabox_post_styles_actions' );
+
+/**
+ * Callback defined in includes/admin/metabox-post-styles.php
+ *
+ * @see carelib_metabox_post_styles_actions
+ */
 add_action( 'load-post-new.php', 'carelib_metabox_post_styles_actions' );
 
-add_action( 'load-post.php',     'carelib_metabox_post_layouts_actions' );
-add_action( 'load-post-new.php', 'carelib_metabox_post_layouts_actions' );
-add_action( 'add_meta_boxes',    'carelib_maybe_disable_post_layout_metabox', 5, 2 );
+/**
+ * Callback defined in includes/admin/metabox-post-layouts.php
+ *
+ * @see carelib_metabox_post_layouts_actions
+ */
+add_action( 'load-post.php', 'carelib_metabox_post_layouts_actions' );
 
+/**
+ * Callback defined in includes/admin/metabox-post-layouts.php
+ *
+ * @see carelib_metabox_post_layouts_actions
+ */
+add_action( 'load-post-new.php', 'carelib_metabox_post_layouts_actions' );
+
+/**
+ * Callback defined in includes/admin/metabox-post-layouts.php
+ *
+ * @see carelib_maybe_disable_post_layout_metabox
+ */
+add_action( 'add_meta_boxes','carelib_maybe_disable_post_layout_metabox', 5, 2 );
+
+/**
+ * Callback defined in includes/admin/dashboard.php
+ *
+ * @see carelib_maybe_add_theme_dashboard
+ */
 add_action( 'init', 'carelib_maybe_add_theme_dashboard' );
