d16c00ba7cdf7c6c33d42bf56f2ba2bd385a458e
robneu <rob@wpsitecare.com>
2017-01-11 16:28:29 -0500

    Removed template hierarchy file

--- includes/filters.php
+++ includes/filters.php
@@ -241,14 +241,14 @@ add_filter( 'carelib_attr_comment-published', 'carelib_attr_comment_published', 
 add_filter( 'carelib_attr_comment-permalink', 'carelib_attr_comment_permalink', 5 );
 
 /**
- * Callback defined in includes/template-hierarchy.php
+ * Callback defined in includes/template-load.php
  *
  * @see carelib_index_include
  */
 add_filter( 'template_include', 'carelib_index_include', 95 );
 
 /**
- * Callback defined in includes/template-hierarchy.php
+ * Callback defined in includes/template-load.php
  *
  * Doesn't work b/c bug with get_query_template().
  *
@@ -257,14 +257,14 @@ add_filter( 'template_include', 'carelib_index_include', 95 );
 add_filter( 'front_page_template', 'carelib_front_page_template', 5 );
 
 /**
- * Callback defined in includes/template-hierarchy.php
+ * Callback defined in includes/template-load.php
  *
  * @see carelib_front_page_template
  */
 add_filter( 'frontpage_template', 'carelib_front_page_template', 5 );
 
 /**
- * Callback defined in includes/template-hierarchy.php
+ * Callback defined in includes/template-comments.php
  *
  * @see carelib_comments_template
  */
